6eadf0284edfcf47031557becefecd8c089a87cc
danikkrapivnitskit <danikkrapivnitskiy02007@gmail.com>
2023-08-22 23:33:20 +0200

    Update Chrome driver setup

--- src/main/java/com/regiojet/DriverFactory.java
+++ src/main/java/com/regiojet/DriverFactory.java
@@ -3,14 +3,17 @@ package com.regiojet;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class DriverFactory {
 
     public static WebDriver getDriver(String browser) throws Exception {
         if (browser.equals("chrome")) {
-            WebDriverManager.chromedriver().setup();
-            return new ChromeDriver();
+            System.setProperty("webdriver.chrome.driver", "driver\\chromedriver-win64\\chromedriver.exe");
+            ChromeOptions co = new ChromeOptions();
+            co.setBinary("driver\\chrome-win64\\chrome.exe");
+            return new ChromeDriver(co);
         } else if (browser.equals("firefox")) {
             WebDriverManager.firefoxdriver().setup();
             return new FirefoxDriver();
