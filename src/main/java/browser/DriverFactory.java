ac4b71e1a6536e26556482d58c9798648d05699e
daniil.krapiunitski <daniil.krapiunitski@certicon.cz>
2023-08-23 13:36:30 +0200

    Remove commit reset

--- src/main/java/browser/DriverFactory.java
+++ src/main/java/browser/DriverFactory.java
@@ -7,17 +7,19 @@ import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class DriverFactory {
+    static WebDriver driver;
 
     public static WebDriver getDriver(String browser) throws Exception {
         if (browser.equals("chrome")) {
             System.setProperty("webdriver.chrome.driver", "driver\\chromedriver-win64\\chromedriver.exe");
             ChromeOptions co = new ChromeOptions();
             co.setBinary("driver\\chrome-win64\\chrome.exe");
-            return new ChromeDriver(co);
+            driver = new ChromeDriver(co);
         } else if (browser.equals("firefox")) {
             WebDriverManager.firefoxdriver().setup();
-            return new FirefoxDriver();
+            driver = new FirefoxDriver();
         } else throw new Exception("Not correct browser");
+        return driver;
     }
 
 }
