c5e63bb92db1425f2a8a317f033a79ad52c06cc4
daniil.krapiunitski <daniil.krapiunitski@certicon.cz>
2023-08-23 16:54:54 +0200

    Remove unused locators

--- src/main/java/cz/idos/LocatorsIdos.java
+++ src/main/java/cz/idos/LocatorsIdos.java
@@ -14,17 +14,7 @@ public class LocatorsIdos {
     public final By priceErrorMsg = By.xpath("//span[@title = 'Cenu se nepodařilo získat']");
     public final By timeAndDateDeparture = By.xpath("(//h2[@class='reset date'])");
     public final By time = By.xpath("(//p//input[@autocomplete='off'])[2]");
-    public final By confirm = By.xpath("//div[@class='react-select__option css-1oxt1zm-option'][1]");
     public final By tomorrow = By.xpath("//a[@class='inc ico-right pikaday-up btn btn-blue']");
-    public final By today = By.xpath("(//div[@class='inline-block p-3 searchBox-mob-container']//button)[1]");
     public final By search = By.xpath("(//button[@class='btn btn-orange btn-small btn-shadow w-full '])");
-    public final By departure = By.xpath("//div[@class='flex items-center gap-1']");
     public final By acceptCookies = By.id("didomi-notice-agree-button");
-    public final By timeDuration = By.xpath("//span[@class='text-13 lg:text-14']");
-    public final By selectItem = By.xpath("(//span[@class='text-13 lg:text-14']//../../div[2])");
-    public final By timeDepartureAndArrival = By.xpath("(//h2[@class='h3'])");
-    public final By soldOut = By.xpath("//div[@class='flex items-center text-13 lg:text-14 flex-wrap text-secondary-redwarn']");
-    public final By mondayDeparture = By.xpath("(//td[@class='CalendarDay CalendarDay_1 CalendarDay__default " +
-            "CalendarDay__default_2 CalendarDay__firstDayOfWeek CalendarDay__firstDayOfWeek_3'])[1]");
-
 }
